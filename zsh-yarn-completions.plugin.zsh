e0f0f0634271972f1b13ecff7b65279114e3d093
Grigorii Horos <horosgrisa@gmail.com>
2023-08-03 12:16:43 +0300

    Optimize plugin

--- zsh-yarn-completions.plugin.zsh
+++ zsh-yarn-completions.plugin.zsh
@@ -1,2 +1,10 @@
-0=${(%):-%N}
-source ${0:A:h}/zsh-yarn-completions.zsh
+#!/usr/bin/env zsh
+
+# Standarized $0 handling, following:
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
+}
